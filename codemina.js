e4353d47cfb8cb4e3b5d90874aa04bda595a9374
Ibrahim Haj <e.ibh2011@gmail.com>
2020-06-25 16:51:26 +0300

    update codemina js safe mode

--- codemina.js
+++ codemina.js
@@ -44,48 +44,52 @@ const CodeMina = function () {
             jQuery(this).addClass('js-numeric-enabled');
         });
 
-        jQuery('.js-numbers-only:not(.js-numbers-only-enabled)')
-            .addClass('js-numbers-only-enabled')
-            .numeric({
-                negative: false,
-            });
+        jQuery('.js-numbers-only:not(.js-numbers-only-enabled)').each(function() {
+            $(this)
+                .addClass('js-numbers-only-enabled')
+                .numeric({
+                    negative: false,
+                });
+        });
     };
 
     let uiHelperMultiSelect = function () {
-        jQuery('.js-multiselect:not(.js-multiselect-enabled)').multiSelect({
-            selectableHeader: "<input type='text' class='form-control' autocomplete='off' placeholder='Search' style='margin-bottom: 10px'>",
-            selectionHeader: "<input type='text' class='form-control' autocomplete='off' placeholder='Search selected' style='margin-bottom: 10px'>",
-            afterInit: function(ms){
-                let that = this,
-                    $selectableSearch = that.$selectableUl.prev(),
-                    $selectionSearch = that.$selectionUl.prev(),
-                    selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)',
-                    selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
+        jQuery('.js-multiselect:not(.js-multiselect-enabled)').each(function() {
+            $(this).multiSelect({
+                selectableHeader: "<input type='text' class='form-control' autocomplete='off' placeholder='Search' style='margin-bottom: 10px'>",
+                selectionHeader: "<input type='text' class='form-control' autocomplete='off' placeholder='Search selected' style='margin-bottom: 10px'>",
+                afterInit: function(ms){
+                    let that = this,
+                        $selectableSearch = that.$selectableUl.prev(),
+                        $selectionSearch = that.$selectionUl.prev(),
+                        selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)',
+                        selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
 
-                that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
-                    .on('keydown', function(e){
-                        if (e.which === 40){
-                            that.$selectableUl.focus();
-                            return false;
-                        }
-                    });
+                    that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
+                        .on('keydown', function(e){
+                            if (e.which === 40){
+                                that.$selectableUl.focus();
+                                return false;
+                            }
+                        });
 
-                that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
-                    .on('keydown', function(e){
-                        if (e.which === 40){
-                            that.$selectionUl.focus();
-                            return false;
-                        }
-                    });
-            },
-            afterSelect: function(){
-                this.qs1.cache();
-                this.qs2.cache();
-            },
-            afterDeselect: function(){
-                this.qs1.cache();
-                this.qs2.cache();
-            },
+                    that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
+                        .on('keydown', function(e){
+                            if (e.which === 40){
+                                that.$selectionUl.focus();
+                                return false;
+                            }
+                        });
+                },
+                afterSelect: function(){
+                    this.qs1.cache();
+                    this.qs2.cache();
+                },
+                afterDeselect: function(){
+                    this.qs1.cache();
+                    this.qs2.cache();
+                },
+            });
         });
     };
 
@@ -121,9 +125,11 @@ const CodeMina = function () {
     };
 
     let uiHelperTimePicker = function () {
-        jQuery('.js-timepicker:not(.js-timepicker-enabled)')
-            .addClass('js-timepicker-enabled')
-            .timepicker();
+        jQuery('.js-timepicker:not(.js-timepicker-enabled)').each(function() {
+            $(this)
+                .addClass('js-timepicker-enabled')
+                .timepicker();
+        });
     };
 
     let uiHelperDisableAutoComplete = function () {
